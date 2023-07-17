5ed6c35f47196b7fc542a13b5efecd6e818aa2eb
AbdullaArif <arifabdulla034@gmail.com>
2023-07-17 16:43:43 +0400

    Program.cs,DbContext

--- Portfolio/DAL/AppDbContext.cs
+++ Portfolio/DAL/AppDbContext.cs
@@ -1,7 +1,12 @@
-﻿namespace Portfolio.DAL
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Portfolio.DAL
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
-      
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option)
+        {
+
+        }
     }
 }
