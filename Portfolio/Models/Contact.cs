20c7c56feca46f01d08f06b2b6f4db2b4be5804b
AbdullaArif <arifabdulla034@gmail.com>
2023-07-23 01:18:49 +0400

    Contact Message To Admin

---
+++ Portfolio/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    public class Contact
+    {
+        public string? FullName { get; set; }
+        public string? Phone { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
