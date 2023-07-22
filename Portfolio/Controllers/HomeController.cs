69da0f5456ac3651020f7aee3a163bed1fc11790
AbdullaArif <arifabdulla034@gmail.com>
2023-07-23 01:25:58 +0400

    Contact Post

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.DAL;
 using Portfolio.Models;
 using System.Diagnostics;
 
@@ -6,13 +7,36 @@ namespace Portfolio.Controllers
 {
     public class HomeController : Controller
     {
-       
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
 
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult Index(Contact contact)
+        {
+            Contact newMessage = new Contact()
+            {
+                FullName = contact.FullName,
+                Phone = contact.Phone,
+                Message = contact.Message,
+                Email = contact.Email,
+            };
+            if (string.IsNullOrEmpty(contact.Email))
+            {
+                ModelState.AddModelError(nameof(contact.Email), "Email is required.");
+                return View(contact);
+            }
 
-             
+            await _context.Contacts.AddAsync(newMessage);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
