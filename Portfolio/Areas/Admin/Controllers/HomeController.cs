ef2bb20e7d1104d0e1acf1f5ed1e5f6a01457d2d
AbdullaArif <arifabdulla034@gmail.com>
2023-07-25 23:02:14 +0400

    Admin Controller

--- Portfolio/Areas/Admin/Controllers/HomeController.cs
+++ Portfolio/Areas/Admin/Controllers/HomeController.cs
@@ -1,13 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Configuration;
+using Portfolio.DAL;
+using Portfolio.Models;
+using System.Data;
 
 namespace Portfolio.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Contact> contacts = await _context.Contacts.ToListAsync();
+            return View(contacts);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id is null || id < 1) return BadRequest();
+
+            Contact contacts = await _context.Contacts.FirstOrDefaultAsync(s => s.Id == id);
+
+            if (contacts is null) return NotFound();
+            _context.Contacts.Remove(contacts);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
